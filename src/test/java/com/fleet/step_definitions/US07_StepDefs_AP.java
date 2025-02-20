e6b2d3807dd8fca98cc7ca930d81fb51c3b45cb9
aysel <ayselarmy@hotmail.com>
2025-02-20 13:00:28 -0500

    US07_B38G_370

--- src/test/java/com/fleet/step_definitions/US07_StepDefs_AP.java
+++ src/test/java/com/fleet/step_definitions/US07_StepDefs_AP.java
@@ -1,84 +1,70 @@
 package com.fleet.step_definitions;
 
-import com.fleet.pages.US07Page_AP;
+import com.fleet.pages.US07_VehiclesPage_AP;
 import com.fleet.pages.LoginPage;
 import com.fleet.utilities.BrowserUtils;
 import com.fleet.utilities.ConfigurationReader;
-import com.fleet.utilities.Driver;
 import io.cucumber.java.en.And;
-import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
+import org.junit.Assert;
 import org.openqa.selenium.Keys;
 
-    public class US07_StepDefs_AP {
+import java.util.List;
 
+public class US07_StepDefs_AP {
 
-        @Given("user is on the login page")
-        public void user_is_on_the_login_page() {
-            Driver.getDriver().get("https://qa3.vytrack.com/");
-
-        }
 
         LoginPage loginPage = new LoginPage();
 
-        @When("the user clicks the login button")
+        @When("the user clicks the login button AP")
         public void user_enters_the_driver_information() {
             loginPage.userName.sendKeys(ConfigurationReader.getProperty("sales_manager_username") + Keys.ENTER);
             loginPage.password.sendKeys(ConfigurationReader.getProperty("sales_manager_password") + Keys.ENTER);
-        }
 
-        @Then("the user clicks the fleet options")
+        }
+        @Then("the user clicks the fleet options AP")
         public void theUserClicksTheFleetOptions() {
 
-            US07Page_AP checkboxesAPPage = new US07Page_AP();
-            checkboxesAPPage.fleetButton.click();
+            US07_VehiclesPage_AP checkboxesAPPage = new US07_VehiclesPage_AP();
+            checkboxesAPPage.fleetButtonAP.click();
             BrowserUtils.sleep(3);
 
         }
 
 
-        @Then("the user should be able to select any vehicle from the Vehicles page")
+        @Then("the user should be able to select any vehicle from the Vehicles page AP")
         public void theUserShouldBeAbleToSelectAnyVehicleFromTheVehiclesPage() {
 
-            US07Page_AP checkboxesAPPage = new US07Page_AP();
-            checkboxesAPPage.VehiclesButton.click();
+            US07_VehiclesPage_AP checkboxesAPPage = new US07_VehiclesPage_AP();
+            checkboxesAPPage.VehiclesButtonAP.click();
 
         }
 
-        @And("user can check the first checkbox to select all cars")
+        @And("user can check the first checkbox to select all cars AP")
         public void userCanCheckTheFirstCheckboxToSelectAllCars() {
 
-            US07Page_AP checkboxesAPPage = new US07Page_AP();
-            checkboxesAPPage.checkbox.click();
+            US07_VehiclesPage_AP checkboxesAPPage = new US07_VehiclesPage_AP();
+            checkboxesAPPage.checkboxAP.click();
 
             BrowserUtils.sleep(2);
-            checkboxesAPPage.clickButton.click();
-
-
-
+            checkboxesAPPage.clickButtonAP.click();
         }
 
-        @Then("user clear the checkbox to select any car")
+        @Then("user clear the checkbox to select any car AP")
         public void userClearTheCheckboxToSelectAnyCar() {
-            US07Page_AP checkboxesAPPage = new US07Page_AP();
-            checkboxesAPPage.groupButton.click();
+            US07_VehiclesPage_AP checkboxesAPPage = new US07_VehiclesPage_AP();
+            checkboxesAPPage.groupButtonAP.click();
             BrowserUtils.sleep(2);
-            //    checkboxesAPPage.dropdownMenu.click();*/
-
-
         }
 
 
-        @Then("user can select any car")
+        @Then("user can select any car AP")
         public void userCanSelectAnyCar() {
 
-            US07Page_AP checkboxesAPPage = new US07Page_AP();
-            checkboxesAPPage.selectAny.click();
+            US07_VehiclesPage_AP checkboxesAPPage = new US07_VehiclesPage_AP();
+            checkboxesAPPage.selectAnyAP.click();
             BrowserUtils.sleep(2);
-
-
-
 
         }
 
--- src/test/resources/features/US07_AP.feature
+++ src/test/resources/features/US07_AP.feature
@@ -4,10 +4,9 @@ Feature: Vehicle Selection from Vehicle Pace
   Scenario: As a user should be able to select any
     vehicle from the Vehicles page
 
-    Given the user is on the login page
-    Then the user clicks the login button
-    Then the user clicks the fleet options
-    Then the user should be able to select any vehicle from the Vehicles page
-    And user can check the first checkbox to select all cars
-    Then user clear the checkbox to select any car
-    Then user can select any car
+    Then the user clicks the login button AP
+    Then the user clicks the fleet options AP
+    Then the user should be able to select any vehicle from the Vehicles page AP
+    And user can check the first checkbox to select all cars AP
+    Then user clear the checkbox to select any car AP
+    Then user can select any car AP
